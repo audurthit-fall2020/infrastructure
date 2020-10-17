746b82cd14885a22f648292043f3756ac59f9ac1
Trivedh Audurthi <audurthi.t@northeastern.edu>
2020-10-17 17:11:56 -0400

    added resources for asgn5

--- 04/main.tf
+++ 04/main.tf
@@ -72,4 +72,196 @@ resource "aws_route_table_association" "rt_sb3" {
   subnet_id      = aws_subnet.sb3.id
   route_table_id = aws_route_table.rt.id
 }
+resource "aws_security_group" "application" {
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
 
+  ingress {
+    description = "TLS"
+    from_port   = var.appSgPorts["tls"]
+    to_port     = var.appSgPorts["tls"]
+    protocol    = "tcp"
+    cidr_blocks = [var.appSgCidr]
+  }
+  ingress {
+    description = "application port"
+    from_port   = var.appSgPorts["app"]
+    to_port     = var.appSgPorts["app"]
+    protocol    = "tcp"
+    cidr_blocks = [var.appSgCidr]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = var.appSgPorts["ssh"]
+    to_port     = var.appSgPorts["ssh"]
+    protocol    = "tcp"
+    cidr_blocks = [var.appSgCidr]
+  }
+  ingress {
+    description = "Http"
+    from_port   = var.appSgPorts["http"]
+    to_port     = var.appSgPorts["http"]
+    protocol    = "tcp"
+    cidr_blocks = [var.appSgCidr]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "application"
+  }
+}
+resource "aws_security_group" "database" {
+  description = "MySQL on RDS"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "TLS"
+    from_port   = var.mysql_port
+    to_port     = var.mysql_port
+    protocol    = "tcp"
+    security_groups=[
+      aws_security_group.application.name
+    ]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "application"
+  }
+}
+resource "aws_s3_bucket" "bucket" {
+  bucket = "webapp.trivedh.audurthi"
+  force_destroy=true
+  lifecycle_rule {
+    enabled=true
+    transition {
+      days=var.s3_webapp_bucket["days"]
+      storage_class=var.s3_webapp_bucket["storage_class"]
+    }
+    noncurrent_version_transition {
+      days=var.s3_webapp_bucket["days"]
+      storage_class=var.s3_webapp_bucket["storage_class"]
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm= "AES256"
+      }
+    }
+  }
+  versioning {
+    enabled = true
+  }
+}
+resource "aws_db_subnet_group" "db_subnet_grp" {
+  subnet_ids = [aws_subnet.sb1.id, aws_subnet.sb2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+resource "aws_db_instance" "rds" {
+  multi_az             = var.rds_instance["multi_az"]
+  identifier           = var.rds_instance["identifier"]
+  allocated_storage    = var.rds_instance["storage"]
+  engine               = var.rds_instance["engine"]
+  engine_version       = var.rds_instance["engine_version"]
+  instance_class       = var.rds_instance["instance_class"]
+  name                 = var.rds_instance["dbname"]
+  username             = var.rds_instance["username"]
+  password             = var.rds_instance["password"]
+  db_subnet_group_name = aws_db_subnet_group.db_subnet_grp.id
+  security_group_names = [aws_security_group.database.name]
+}
+data "template_file" "cloud_init" {
+  template = file("${path.module}/cloudconfig.yml")
+  vars = {
+    rds_hostname = aws_db_instance.rds.address
+  }
+}
+resource "aws_instance" "web" {
+  ami                  = var.ec2_instance["ami"]
+  instance_type        = var.ec2_instance["instance_type"]
+  root_block_device {
+    volume_size   = var.ec2_instance["volume_size"]
+    delete_on_termination= var.ec2_instance["delete_on_termination"]
+    volume_type   = var.ec2_instance["volume_type"]
+  }
+  vpc_security_group_ids = [aws_security_group.application.name]
+  subnet_id              = aws_subnet.sb3.id
+  user_data              = data.template_file.cloud_init.rendered
+  iam_instance_profile   = aws_iam_role.ec2_instance_role.name
+}
+resource "aws_dynamodb_table" "dynamoDB"{
+  name       = var.dynamoDB["name"]
+  hash_key   = var.dynamoDB["hash_key"]
+  attribute {
+    name = var.dynamoDB["hash_key"]
+    type = var.dynamoDB["hash_key_type"]
+  }
+}
+resource "aws_iam_policy" "s3_iam_policy" {
+  name        = "WebAppS3"
+  description = "S3 IAM policy for application access"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+              "s3:CreateBucket",
+              "s3:DeleteBucket",
+              "s3:DeleteObject",
+              "s3:DeleteObjectVersion",
+              "s3:GetLifecycleConfiguration",
+              "s3:GetObject",
+              "s3:GetObjectVersion",
+              "s3:PutBucketAcl",
+              "s3:PutEncryptionConfiguration",
+              "s3:PutLifecycleConfiguration",
+              "s3:PutObject"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::webapp.trivedh.audurthi",
+                "arn:aws:s3:::webapp.trivedh.audurthi/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "EC2-CSYE6225"
+  assume_role_policy = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow"
+        }
+      ]
+    }
+EOF
+}
+resource "aws_iam_role_policy_attachment" "ec2_s3_attachment" {
+  role       = aws_iam_role.ec2_instance_role.name
+  policy_arn = aws_iam_policy.s3_iam_policy.arn
+}
--- 04/variables.tf
+++ 04/variables.tf
@@ -26,3 +26,59 @@ variable "ig_cidr"{
     type=string
     default="0.0.0.0/0"
 }
+variable "appSgPorts"{
+    type=map
+    default={
+        "tls"=443,
+        "app"=5000,
+        "ssh"=22,
+        "http"=80
+    }
+}
+variable "appSgCidr"{
+    type=string
+    default="0.0.0.0/0"
+}
+variable "mysql_port"{
+    type=number
+    default=3306
+}
+variable "s3_webapp_bucket"{
+    type=map
+    default={
+        "days"=30
+        "storage_class"="STANDARD_IA"
+    }
+}
+variable "rds_instance"{
+    type=map
+    default={
+        "multi_az"=false
+        "storage"=20
+        "identifier"= "csye6225-f20"
+        "engine"="mysql"
+        "engine_version"="5.7"
+        "instance_class"= "db.t3.micro"
+        "dbname"= "csye6225"
+        "username"= "csye6225fall2020"
+        "password"= "Test@1234"
+    }
+}
+variable "ec2_instance"{
+    type=map
+    default={
+        "ami"="abc"
+        "delete_on_termination" =true
+        "instance_type"="t2.micro"
+        "volume_size"=20
+        "volume_type"="gp2"
+    }
+}
+variable "dynamoDB"{
+    type=map
+    default={
+        "name"="csye6225"
+        "hash_key"="id"
+        "hash_key_type"="S"
+    }
+}
